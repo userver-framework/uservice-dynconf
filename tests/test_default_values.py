c85f3135aa5840580eeb63e28de3cf10541ddf46
Antony Polukhin <antoshkka@yandex-team.ru>
2022-08-29 12:37:14 +0300

    format

--- tests/test_default_values.py
+++ tests/test_default_values.py
@@ -2,6 +2,10 @@ import pytest
 
 from testsuite.databases import pgsql
 
+
+# Make sure that the defaults in default_configs.sql match values in
+# configs/dynamic_config_fallback.json and there's no additional values in the
+# latter.
 @pytest.mark.pgsql('uservice_dynconf', files=['default_configs.sql'])
 async def test_default_values(pgsql, load_json):
     cursor = pgsql['uservice_dynconf'].cursor()
@@ -13,9 +17,9 @@ async def test_default_values(pgsql, load_json):
     assert len(data) == 1
     assert len(data[0]) == 1
     db_defaults = data[0][0]
-    
+
     service_defaults = load_json('configs/dynamic_config_fallback.json')
-    
+
     for key, value in service_defaults.items():
         assert key in db_defaults
         assert db_defaults[key] == value, 'At key: ' + key
