fb0f55031968fb530cc9fa85c54ccad7a71b0841
p2034 <kandakovdr.theend@gmail.com>
2022-11-12 17:22:38 +0300

    db and api

--- postgresql/schemas/uservice_dynconf.sql
+++ postgresql/schemas/uservice_dynconf.sql
@@ -1,5 +1,26 @@
 DROP SCHEMA IF EXISTS uservice_dynconf CASCADE;
 CREATE SCHEMA IF NOT EXISTS uservice_dynconf;
+
+CREATE TABLE IF NOT EXISTS uservice_dynconf.configs (
+    service TEXT NOT NULL DEFAULT '__default__',
+    config_name TEXT NOT NULL,
+    config_value JSONB,
+    created_at timestamptz DEFAULT NOW(),
+    updated_at timestamptz DEFAULT NOW(),
+
+    PRIMARY KEY (service, config_name)
+);
+
+CREATE INDEX IF NOT EXISTS idx__created_at__configs
+ON uservice_dynconf.configs USING btree (created_at);
+
+CREATE INDEX IF NOT EXISTS idx__updated_at__configs
+ON uservice_dynconf.configs USING btree (updated_at);
+
+CREATE UNIQUE INDEX IF NOT EXISTS idx__pair_service_and_connfig
+ON uservice_dynconf.configs USING btree (service, config_name);
+
+-- new 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS uservice_dynconf.variables (
@@ -20,7 +41,7 @@ ON uservice_dynconf.variables USING btree ("created_at");
 CREATE INDEX IF NOT EXISTS idx__updated_at__configs
 ON uservice_dynconf.variables USING btree ("updated_at");
 
-CREATE TABLE IF NOT EXISTS uservice_dynconf.configs (
+CREATE TABLE IF NOT EXISTS uservice_dynconf.configs_ (
     "uuid" TEXT PRIMARY KEY DEFAULT uuid_generate_v4(),
     "name" TEXT
 );
@@ -28,17 +49,8 @@ CREATE TABLE IF NOT EXISTS uservice_dynconf.configs (
 CREATE TABLE IF NOT EXISTS uservice_dynconf.users_x_configs (
     "user" TEXT NOT NULL,
     "config" TEXT NOT NULL,
-    CONSTRAINT fk_users_x_configs__user
-		FOREIGN KEY("user")
-			REFERENCES uservice_dynconf.users("uuid"),
     CONSTRAINT fk_users_x_configs__config
 		FOREIGN KEY("config")
-			REFERENCES uservice_dynconf.configs("uuid")
+			REFERENCES uservice_dynconf.configs_("uuid")
     "permission" TEXT
-);
-
-CREATE TABLE IF NOT EXISTS uservice_dynconf.users (
-    "uuid" TEXT PRIMARY KEY DEFAULT uuid_generate_v4(),
-    "login" TEXT NOT NULL UNIQUE,
-    "password_hash" TEXT NOT NULL
 );
