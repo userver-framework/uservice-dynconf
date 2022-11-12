0c6560429baee013f8a8601e791787a08e7ffa09
Quviry <kogekao@ya.ru>
2022-11-12 18:09:11 +0300

    Database table userxs added

--- postgresql/schemas/uservice_dynconf.sql
+++ postgresql/schemas/uservice_dynconf.sql
@@ -1,7 +1,10 @@
 DROP SCHEMA IF EXISTS uservice_dynconf CASCADE;
 CREATE SCHEMA IF NOT EXISTS uservice_dynconf;
 
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE IF NOT EXISTS uservice_dynconf.configs (
+    uuid TEXT NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
     service TEXT NOT NULL DEFAULT '__default__',
     config_name TEXT NOT NULL,
     config_value JSONB,
@@ -20,37 +23,9 @@ ON uservice_dynconf.configs USING btree (updated_at);
 CREATE UNIQUE INDEX IF NOT EXISTS idx__pair_service_and_connfig
 ON uservice_dynconf.configs USING btree (service, config_name);
 
--- new 
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
-CREATE TABLE IF NOT EXISTS uservice_dynconf.variables (
-    "uuid" TEXT PRIMARY KEY DEFAULT uuid_generate_v4(),
-    "config" TEXT,
-    "name" TEXT NOT NULL,
-    "value" JSONB,
-    "created_at" timestamptz DEFAULT NOW(),
-    "updated_at" timestamptz DEFAULT NOW(),
-    CONSTRAINT fk_variables__config
-		FOREIGN KEY("config")
-			REFERENCES uservice_dynconf.configs_(uuid)
-);
-
-CREATE INDEX IF NOT EXISTS idx__created_at__configs
-ON uservice_dynconf.variables USING btree ("created_at");
-
-CREATE INDEX IF NOT EXISTS idx__updated_at__configs
-ON uservice_dynconf.variables USING btree ("updated_at");
-
-CREATE TABLE IF NOT EXISTS uservice_dynconf.configs_ (
-    "uuid" TEXT PRIMARY KEY DEFAULT uuid_generate_v4(),
-    "name" TEXT
-);
 
 CREATE TABLE IF NOT EXISTS uservice_dynconf.users_x_configs (
     "user" TEXT NOT NULL,
-    "config" TEXT NOT NULL,
-    CONSTRAINT fk_users_x_configs__config
-		FOREIGN KEY("config")
-			REFERENCES uservice_dynconf.configs_("uuid")
-    "permission" TEXT
+    service TEXT NOT NULL,
+    permission TEXT
 );
