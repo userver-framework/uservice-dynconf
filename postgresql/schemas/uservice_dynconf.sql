12602b49bc610e6be1de4a40c773f81b57b06e5a
p2034 <kandakovdr.theend@gmail.com>
2022-11-12 14:07:20 +0300

    update database add users_x_configs

--- postgresql/schemas/uservice_dynconf.sql
+++ postgresql/schemas/uservice_dynconf.sql
@@ -3,30 +3,42 @@ CREATE SCHEMA IF NOT EXISTS uservice_dynconf;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS uservice_dynconf.variables (
-    uuid TEXT PRIMARY KEY DEFAULT uuid_generate_v4(),
-    "service" TEXT,
+    "uuid" TEXT PRIMARY KEY DEFAULT uuid_generate_v4(),
+    "config" TEXT,
     "name" TEXT NOT NULL,
     "value" JSONB,
-    created_at timestamptz DEFAULT NOW(),
-    updated_at timestamptz DEFAULT NOW(),
-    CONSTRAINT fk_service
-		FOREIGN KEY("service")
+    "created_at" timestamptz DEFAULT NOW(),
+    "updated_at" timestamptz DEFAULT NOW(),
+    CONSTRAINT fk_variables__config
+		FOREIGN KEY("config")
 			REFERENCES uservice_dynconf.configs(uuid)
 );
 
 CREATE INDEX IF NOT EXISTS idx__created_at__configs
-ON uservice_dynconf.variables USING btree (created_at);
+ON uservice_dynconf.variables USING btree ("created_at");
 
 CREATE INDEX IF NOT EXISTS idx__updated_at__configs
-ON uservice_dynconf.variables USING btree (updated_at);
+ON uservice_dynconf.variables USING btree ("updated_at");
 
 CREATE TABLE IF NOT EXISTS uservice_dynconf.configs (
-    uuid TEXT PRIMARY KEY DEFAULT uuid_generate_v4(),
+    "uuid" TEXT PRIMARY KEY DEFAULT uuid_generate_v4(),
     "name" TEXT
 );
 
+CREATE TABLE IF NOT EXISTS uservice_dynconf.users_x_configs (
+    "user" TEXT NOT NULL,
+    "config" TEXT NOT NULL,
+    CONSTRAINT fk_users_x_configs__user
+		FOREIGN KEY("user")
+			REFERENCES uservice_dynconf.users("uuid"),
+    CONSTRAINT fk_users_x_configs__config
+		FOREIGN KEY("config")
+			REFERENCES uservice_dynconf.configs("uuid")
+    "permission" TEXT
+);
+
 CREATE TABLE IF NOT EXISTS uservice_dynconf.users (
-    uuid TEXT PRIMARY KEY DEFAULT uuid_generate_v4(),
+    "uuid" TEXT PRIMARY KEY DEFAULT uuid_generate_v4(),
     "login" TEXT NOT NULL UNIQUE,
-    password_hash TEXT NOT NULL
+    "password_hash" TEXT NOT NULL
 );
