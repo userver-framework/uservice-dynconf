cdd383c934c7b8c844e31d5a06dd5049cec64f75
p2034 <kandakovdr.theend@gmail.com>
2022-11-12 13:40:54 +0300

    update database

--- postgresql/schemas/uservice_dynconf.sql
+++ postgresql/schemas/uservice_dynconf.sql
@@ -1,23 +1,32 @@
 DROP SCHEMA IF EXISTS uservice_dynconf CASCADE;
-
 CREATE SCHEMA IF NOT EXISTS uservice_dynconf;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-
-CREATE TABLE IF NOT EXISTS uservice_dynconf.configs (
-    service TEXT NOT NULL DEFAULT '__default__',
-    config_name TEXT NOT NULL,
-    config_value JSONB,
+CREATE TABLE IF NOT EXISTS uservice_dynconf.variables (
+    uuid TEXT PRIMARY KEY DEFAULT uuid_generate_v4(),
+    "service" TEXT,
+    "name" TEXT NOT NULL,
+    "value" JSONB,
     created_at timestamptz DEFAULT NOW(),
     updated_at timestamptz DEFAULT NOW(),
-
-    PRIMARY KEY (service, config_name)
+    CONSTRAINT fk_service
+		FOREIGN KEY("service")
+			REFERENCES uservice_dynconf.configs(uuid)
 );
 
 CREATE INDEX IF NOT EXISTS idx__created_at__configs
-ON uservice_dynconf.configs USING btree (created_at);
+ON uservice_dynconf.variables USING btree (created_at);
 
 CREATE INDEX IF NOT EXISTS idx__updated_at__configs
-ON uservice_dynconf.configs USING btree (updated_at);
+ON uservice_dynconf.variables USING btree (updated_at);
 
-CREATE UNIQUE INDEX IF NOT EXISTS idx__pair_service_and_connfig
-ON uservice_dynconf.configs USING btree (service, config_name);
+CREATE TABLE IF NOT EXISTS uservice_dynconf.configs (
+    uuid TEXT PRIMARY KEY DEFAULT uuid_generate_v4(),
+    "name" TEXT
+);
+
+CREATE TABLE IF NOT EXISTS uservice_dynconf.users (
+    uuid TEXT PRIMARY KEY DEFAULT uuid_generate_v4(),
+    "login" TEXT NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL
+);
