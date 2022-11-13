ed13d9330576a2fa3c5b799eb0859b99d38f1f91
condackow-aleck <condackow.aleck@yandex.ru>
2022-11-13 15:36:17 +0300

    Post endpoint

--- src/sql/sql_query.hpp
+++ src/sql/sql_query.hpp
@@ -23,7 +23,6 @@ DELETE FROM uservice_dynconf.configs
 WHERE service = $1 and config_name IN (SELECT unnest($2));
 )~";
 
-<<<<<<< HEAD
 inline constexpr std::string_view kDeleteVariable = R"~(
 DELETE FROM uservice_dynconf.configs
 WHERE uuid=$1;
