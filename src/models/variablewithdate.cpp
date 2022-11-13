7f0011bc30859c9ecf93e7ac6b83fa073c62ed9b
p2034 <kandakovdr.theend@gmail.com>
2022-11-13 20:13:40 +0300

    all added

---
+++ src/models/variablewithdate.cpp
@@ -0,0 +1,13 @@
+#include "variablewithdate.hpp"
+
+namespace uservice_dynconf::models {
+    userver::formats::json::Value Serialize(const VariableWithDate& response,
+                                            userver::formats::serialize::To<userver::formats::json::Value>) {
+        userver::formats::json::ValueBuilder item;
+        item["uuid"] = response.uuid;
+        item["service"] = response.service;
+        item["config_name"] = response.config_name;
+        item["updated_at"] = response.updated_at;
+        return item.ExtractValue();
+    }
+}
