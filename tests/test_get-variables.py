892f814aa627e65b843018aa04e66157919507b0
p2034 <kandakovdr.theend@gmail.com>
2022-11-13 20:16:44 +0300

    debug

--- tests/test_get-variables.py
+++ tests/test_get-variables.py
@@ -44,5 +44,3 @@ async def test_get_ok_with_params(service_client):
     assert response.status == 200
     assert response.json().get('count') == 2
     assert response.json().get('total') == 24
-    # assert response.json().get('items') == [{"uuid":"aad06c83-8fb9-4afe-97b2-06fd03302857","service":"__default__","config_name":"USERVER_TASK_PROCESSOR_PROFILER_DEBUG","updated_at":"2022-11-13T09:13:49.840703+00:00"},
-    #                                         {"uuid":"e4f74c57-d555-481e-b908-a5098b0bead4","service":"__default__","config_name":"USERVER_TASK_PROCESSOR_QOS","updated_at":"2022-11-13T09:13:49.840703+00:00"}]
