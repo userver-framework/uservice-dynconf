651acc20bbf339e6d69291afceac3a3bc2a16d86
condackow-aleck <condackow.aleck@yandex.ru>
2022-11-19 13:22:34 +0300

    Null config value handling and tests

--- tests/test_post_variables.py
+++ tests/test_post_variables.py
@@ -42,7 +42,17 @@ def db_setup(pgsql):
             "service": "my-service",
             "value": "1000"
         }, "CUSTOM_CONFIG_3", id='add int'),
-    ])
+        pytest.param({
+            "name": "CUSTOM_CONFIG_4",
+            "service": "my-service",
+            "value": "{}"
+        }, "CUSTOM_CONFIG_4", id='add empty json'),
+        pytest.param({
+            "name": "CUSTOM_CONFIG_5",
+            "service": "my-service",
+            "value": "null"
+        }, "CUSTOM_CONFIG_5", id='add null'),
+      ])
 async def test_post_variable_positive(service_client,
                                       db_setup, input, conf_name):
     """Add new config variable"""
@@ -59,18 +69,50 @@ async def test_post_variable_positive(service_client,
     [
         pytest.param({
             "value": "true",
-            "service": "my-service",
-        }, "CUSTOM_CONFIG_2", id='add bool'),
-        pytest.param({
-            "name": "CUSTOM_CONFIG_3",
-            "service": "my-service",
-        }, "CUSTOM_CONFIG_3", id='add int'),
+        }, "CUSTOM_CONFIG", id='no config name'),
     ])
-async def test_post_variable_bad_request(service_client,
-                                         db_setup, input, conf_name):
-    """Add new config variable"""
+async def test_post_variable_no_name(service_client,
+                                     input, conf_name):
+    """Request without config name"""
     response = await service_client.post(
         '/admin/v1/variables', json=input
     )
     assert response.status_code == 400
-    assert response.json()["message"] == "Fields 'name', 'value' are required"
+    assert response.json()["message"] == "Config name is required"
+
+
+@pytest.mark.parametrize(
+    'input, conf_name',
+    [
+        pytest.param({
+            "name": "CUSTOM_CONFIG",
+            "value": '{"property1": 10,'
+                     '"property2": '
+                     '{"info": "info"}}',
+        }, "CUSTOM_CONFIG", id='no service name'),
+    ])
+async def test_post_variable_no_service(service_client,
+                                        input, conf_name):
+    """Request without service name"""
+    response = await service_client.post(
+        '/admin/v1/variables', json=input
+    )
+    assert response.status_code == 200
+    assert response.json()["uuid"]
+
+
+@pytest.mark.parametrize(
+    'input, conf_name',
+    [
+        pytest.param({
+            "name": "CUSTOM_CONFIG",
+        }, "CUSTOM_CONFIG", id='no config value'),
+    ])
+async def test_post_variable_no_value(service_client,
+                                      input, conf_name):
+    """Request without config value"""
+    response = await service_client.post(
+        '/admin/v1/variables', json=input
+    )
+    assert response.status_code == 200
+    assert response.json()["uuid"]
