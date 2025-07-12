0a209b93eece557e4b744c484e4c018388d33aaa
Antony Polukhin <antoshkka@gmail.com>
2025-07-12 19:21:06 +0300

    Minor improvements to interface, tests and installation (#64)
    * Highlight input errors in red
    * Install the web-interface files
    * Add smoke test for admin configs getter
    * Fix "Loading data" after remove-all-rows
    * Update CI names to make those usable in userver/Readme.md

--- src/handlers/admin_v1_configs_get.cpp
+++ src/handlers/admin_v1_configs_get.cpp
@@ -1,19 +1,13 @@
 #include <handlers/admin_v1_configs_get.hpp>
 
 #include <string>
-#include <unordered_set>
 
-#include <userver/formats/json/inline.hpp>
-#include <userver/formats/json/value.hpp>
-#include <userver/formats/yaml/value_builder.hpp>
+#include <userver/formats/json/value_builder.hpp>
 #include <userver/storages/postgres/cluster.hpp>
 #include <userver/storages/postgres/component.hpp>
 
 #include <docs/api/api.hpp>
 #include <uservice_dynconf/sql_queries.hpp>
-
-#include <models/config.hpp>
-#include <utils/make_error.hpp>
 
 namespace uservice_dynconf::handlers::admin_v1_configs_get::post {
 
@@ -29,19 +23,18 @@ userver::formats::json::Value Handler::HandleRequestJsonThrow(
     const userver::server::http::HttpRequest& /*request*/,
     const userver::formats::json::Value& /*request_json*/,
     userver::server::request::RequestContext&) const {
+  namespace upg = userver::storages::postgres;
 
   userver::formats::json::ValueBuilder result;
-  const auto data = cluster_->Execute(userver::storages::postgres::ClusterHostType::kSlave,
+  const auto data = cluster_->Execute(upg::ClusterHostType::kSlave,
                     uservice_dynconf::sql::kSelectConfigValues);
   for (const auto& row: data) {
-      result.PushBack( 
-        userver::formats::json::MakeObject(
-            "name", row["config_name"].As<std::string>(),
-            "service", row["service"].As<std::string>(),
-            "value", row["config_value"].As<std::string>(),
-            "is_kill_switch", row["is_kill_switch"].As<bool>()
-        )
-      );
+      AdminConfigsValuesResponseBody element;
+      element.name = row["config_name"].As<std::string>();
+      element.service = row["service"].As<std::string>();
+      element.value = row["config_value"].As<std::string>();
+      element.is_kill_switch = row["is_kill_switch"].As<bool>();
+      result.PushBack(std::move(element));
   }
   return result.ExtractValue();
 }
